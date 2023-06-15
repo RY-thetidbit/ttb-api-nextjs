11b11382ec7568c395a36c59c2a5637ebdcccc97
tidbit <thetidbitcompany@gmail.com>
2023-06-15 11:32:22 +0530

    added

--- src/pages/api/notification-cron/[id].js
+++ src/pages/api/notification-cron/[id].js
@@ -4,107 +4,50 @@ const UserModel = require('../../../models/user');
 import sentOSNotification from "../../../../handler/handleOSPushNotificaiton";
 
 
-let job1 = cron.schedule('24 11 * * *', async() => {
-  console.log("################################## cron1")
-  // Task to be executed
-  //CONNECT TO MONGODB DATABASE 
-  connectDB();
-
-  const user = await UserModel.findOne({ mobile:"+918983712448" });
-  if (!user) {
-    return res.status(400).json({ error: 'User mobile does not exist' });
-  } else {
-    user.countCron = user.countCron?user.countCron+1:1;
-  }
-    
-
-    // UPDATE USER INFO IN DB
-    await user.save();
-  console.log('Cron job executed!');
+let job1 = cron.schedule('30 0 * * *', async() => {
+  await sentOSNotification(0);
+  console.log('Cron job executed!', new Date());
 });
 
-let job2 = cron.schedule('54 5 * * *', async() => {
-  console.log("################################## cron2")
-  // Task to be executed
-  //CONNECT TO MONGODB DATABASE 
-  connectDB();
-
-  const user = await UserModel.findOne({ mobile:"+918983712448" });
-  if (!user) {
-    return res.status(400).json({ error: 'User mobile does not exist' });
-  } else {
-    user.countCron = user.countCron?user.countCron+1:1;
-  }
-    
-
-    // UPDATE USER INFO IN DB
-    await user.save();
-  console.log('Cron job executed!');
+let job2 = cron.schedule('30 3 * * *', async() => {
+  await sentOSNotification(1);
+  console.log('Cron job executed!', new Date());
 });
 
+let job3 = cron.schedule('30 6 * * *', async() => {
+  await sentOSNotification(2);
+  console.log('Cron job executed!', new Date());
+});
 
+let job4 = cron.schedule('30 9 * * *', async() => {
+  await sentOSNotification(3);
+  console.log('Cron job executed!', new Date());
+});
 
+let job5 = cron.schedule('30 12 * * *', async() => {
+  await sentOSNotification(4);
+  console.log('Cron job executed!', new Date());
+});
 
-// const updateUser = async (req, res) => {
+let job6 = cron.schedule('30 15 * * *', async() => {
+  await sentOSNotification(5);
+  console.log('Cron job executed!', new Date());
+});
 
-//   try {
-
-//     //CONNECT TO MONGODB DATABASE 
-//     connectDB();
-
-//     const user = await UserModel.findOne({ mobile:"+918983712448" });
-//     if (!user) {
-//       return res.status(400).json({ error: 'User mobile does not exist' });
-//     } else {
-//       user.count =  user.count?user.count+1:1;
-      
-
-//       // UPDATE USER INFO IN DB
-//       await user.save();
-//       return res.status(200).json({ message: 'User updated' });
-//     }
-//   } catch (e) {
-//     console.log("Method- updateUser :", e);
-//     return res.status(500).json({ error: "Error in updating user." })
-//   }
-
-// }
-
-// const testCron = ()=>{
-//   let job1 = cron.schedule('0 6,12 * * *', async() => {
-//     // Task to be executed
-//     //CONNECT TO MONGODB DATABASE 
-//     connectDB();
-
-//     const user = await UserModel.findOne({ mobile:"+918983712448" });
-//     if (!user) {
-//       return res.status(400).json({ error: 'User mobile does not exist' });
-//     } else {
-//       user.countCron = user.countCron?user.countCron+1:1;
-//     }
-      
-
-//       // UPDATE USER INFO IN DB
-//       await user.save();
-//     console.log('Cron job executed!');
-//   });
-// }
+let job7 = cron.schedule('5 6 * * *', async() => {
+  await sentOSNotification(5);
+  console.log('Cron job executed!', new Date());
+});
 
 export default async function handler(req, res) {
-  job1.start()
-  job2.start()
+  job1.start();
+  job2.start();
+  job3.start();
+  job4.start();
+  job5.start();
+  job6.start();
+  job7.start();
+
   res.status(200).json({ date: new Date() });
-
-  // await updateUser(req, res);
-  // testCron();
   
-  // let { id=0 } = req?.query || {};
-
-  // id = isNaN(parseInt(id))?0:parseInt(id);
-
-  // console.log("reached :")
-  //  await sentOSNotification(id);
-  //  console.log("reached : send")
-
-  // res.status(200).json({ success: true });
 }
