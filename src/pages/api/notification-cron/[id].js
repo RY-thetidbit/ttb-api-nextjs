16204b2fd7843f115676b267b7dd7d512d4dd0b6
tidbit <thetidbitcompany@gmail.com>
2023-06-15 11:08:27 +0530

    cron test heroku

--- src/pages/api/notification-cron/[id].js
+++ src/pages/api/notification-cron/[id].js
@@ -1,33 +1,54 @@
+const cron = require('node-cron');
 import { connectDB } from '../../../db';
 const UserModel = require('../../../models/user');
-const cron = require('node-cron');
-
 import sentOSNotification from "../../../../handler/handleOSPushNotificaiton";
 
-const updateUser = async (req, res) => {
 
-  try {
+let job1 = cron.schedule('* * * * *', async() => {
+  // Task to be executed
+  //CONNECT TO MONGODB DATABASE 
+  connectDB();
 
-    //CONNECT TO MONGODB DATABASE 
-    connectDB();
+  const user = await UserModel.findOne({ mobile:"+918983712448" });
+  if (!user) {
+    return res.status(400).json({ error: 'User mobile does not exist' });
+  } else {
+    user.countCron = user.countCron?user.countCron+1:1;
+  }
+    
 
-    const user = await UserModel.findOne({ mobile:"+918983712448" });
-    if (!user) {
-      return res.status(400).json({ error: 'User mobile does not exist' });
-    } else {
-      user.count =  user.count?user.count+1:1;
+    // UPDATE USER INFO IN DB
+    await user.save();
+  console.log('Cron job executed!');
+});
+
+
+
+
+// const updateUser = async (req, res) => {
+
+//   try {
+
+//     //CONNECT TO MONGODB DATABASE 
+//     connectDB();
+
+//     const user = await UserModel.findOne({ mobile:"+918983712448" });
+//     if (!user) {
+//       return res.status(400).json({ error: 'User mobile does not exist' });
+//     } else {
+//       user.count =  user.count?user.count+1:1;
       
 
-      // UPDATE USER INFO IN DB
-      await user.save();
-      return res.status(200).json({ message: 'User updated' });
-    }
-  } catch (e) {
-    console.log("Method- updateUser :", e);
-    return res.status(500).json({ error: "Error in updating user." })
-  }
+//       // UPDATE USER INFO IN DB
+//       await user.save();
+//       return res.status(200).json({ message: 'User updated' });
+//     }
+//   } catch (e) {
+//     console.log("Method- updateUser :", e);
+//     return res.status(500).json({ error: "Error in updating user." })
+//   }
 
-}
+// }
 
 // const testCron = ()=>{
 //   let job1 = cron.schedule('0 6,12 * * *', async() => {
@@ -50,17 +71,19 @@ const updateUser = async (req, res) => {
 // }
 
 export default async function handler(req, res) {
+  job1.start()
+  res.status(200).json({ date: new Date() });
 
-  await updateUser(req, res);
+  // await updateUser(req, res);
   // testCron();
   
-  let { id=0 } = req?.query || {};
+  // let { id=0 } = req?.query || {};
 
-  id = isNaN(parseInt(id))?0:parseInt(id);
+  // id = isNaN(parseInt(id))?0:parseInt(id);
 
-  console.log("reached :")
+  // console.log("reached :")
   //  await sentOSNotification(id);
-   console.log("reached : send")
+  //  console.log("reached : send")
 
   // res.status(200).json({ success: true });
 }
